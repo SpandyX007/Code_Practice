51c0dbefeb17ef2dd0f7d9cace44653fb22e113a
SpandyX007 <spandanar1234@gmail.com>
2025-09-07 17:51:09 +0530

    GPH#12

--- GPHtopoSort.cpp
+++ GPHtopoSort.cpp
@@ -1,5 +1,5 @@
 // Toposort
-//TC: O(V+E)
+//Using DFS TC: O(V+E)
 
 class Solution {
   public:
@@ -39,6 +39,48 @@ class Solution {
             s.pop();
         }
         
+        return ans;
+    }
+};
+
+// Using Kahn's Algorithm
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+        vector<int> visited(V,0);
+        vector<int> indegree(V,0);
+        
+        for(auto& it:edges)
+        {
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        
+        vector<int> ans;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            // now that node is in the ans, 
+            // remove it from the indegree
+            for(auto it:adj[node])
+            {
+                indegree[it]--; // reducing the connected nodes by 1
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
         return ans;
     }
 };
